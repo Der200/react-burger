a1138a3614fc79e9e85679ab813a384afae31a5c
Der200 <reofwe@gmail.com>
2021-04-26 16:28:58 +0300

    Добавлен стилизованый компонент шапки и моковые данные в utils

---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.module.css';
+import AppHeader from '../app-header/app-header'; 
+
+function App() {
+  return (
+    <AppHeader />
+  );
+}
+
+export default App;
