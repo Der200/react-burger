70e194e85307ec1eea1dbeec3117f4f3ec90c50f
Der200 <reofwe@gmail.com>
2021-06-15 17:34:49 +0300

    fix: scroll to tabs

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components/dist/index.js";
 import styles from './burger-ingredients.module.css';
 import Ingredient from '../ingredient/ingredient';
@@ -16,19 +16,35 @@ const BurgerIngredients = () => {
   } = styles;
 
   const dispatch = useDispatch();
-  const [currentTab, setCurrentTab] = useState('bun');
+  const [currentTab, setCurrentTab] = useState('buns');
+
+  const bunsHeader = useRef();
+  const saucesHeader = useRef();
+  const mainsHeader = useRef();
 
   const [buns, inViewBuns] = useInView({threshold:0.3})
   const [sauces, inViewSauces] = useInView({threshold:1})
   const [mains, inViewMain] = useInView({threshold:0.4})
 
   const handleScrollIngredients = () => {
-    inViewBuns && setCurrentTab('bun')
-    inViewSauces && setCurrentTab('sauce')
-    inViewMain && setCurrentTab('main')
+    inViewBuns && setCurrentTab('buns')
+    inViewSauces && setCurrentTab('sauces')
+    inViewMain && setCurrentTab('mains')
   }
 
+  const scrollToNode = (e) => {
+    if (e === 'buns') {
+      bunsHeader.current.scrollIntoView({behavior: 'smooth'});
+      setCurrentTab('buns')
+    } else if (e === 'sauces') {
+      saucesHeader.current.scrollIntoView({behavior: 'smooth'});
+      setCurrentTab('sauces')
+    } else {
+      mainsHeader.current.scrollIntoView({behavior: 'smooth'});
+      setCurrentTab('mains')
+    }
 
+  }
 
 
   const handleClickIngredient = ingredient => {    
@@ -41,13 +57,13 @@ const BurgerIngredients = () => {
       <section className="mt-10">
         <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
         <div className={[main__tabs, 'tabs'].join(' ')}>
-          <Tab value="bun" active={currentTab === 'bun'} onClick={setCurrentTab}>Булки</Tab>
-          <Tab value="sauce" active={currentTab === 'sauce'} onClick={setCurrentTab}>Соусы</Tab>
-          <Tab value="main" active={currentTab === 'main'} onClick={setCurrentTab}>Начинки</Tab>
+          <Tab value="buns" active={currentTab === 'buns'} onClick={scrollToNode}>Булки</Tab>
+          <Tab value="sauces" active={currentTab === 'sauces'} onClick={scrollToNode}>Соусы</Tab>
+          <Tab value="mains" active={currentTab === 'mains'} onClick={scrollToNode}>Начинки</Tab>
         </div>
       </section>
       <section className={[ingredients__container, assembly__box].join(" ")} onScroll = {handleScrollIngredients} >
-        <h2 className="text text_type_main-medium mb-6 bun">Булки</h2>
+        <h2 className="text text_type_main-medium mb-6 bun" ref={bunsHeader}>Булки</h2>
         <div className={main__ingredients} ref={buns}>
           {ingredients.filter(ingredient => ingredient.type === 'bun').map((ingredient) => (
               <Ingredient
@@ -57,7 +73,7 @@ const BurgerIngredients = () => {
               />
           ))}
         </div>
-        <h2 className="text text_type_main-medium mt-10 mb-6 sauce">Соусы</h2>
+        <h2 className="text text_type_main-medium mt-10 mb-6 sauce" ref={saucesHeader}>Соусы</h2>
         <div className={main__ingredients} ref={sauces}>
           {ingredients.filter(ingredient => ingredient.type === 'sauce').map((ingredient) => (
               <Ingredient
@@ -67,7 +83,7 @@ const BurgerIngredients = () => {
               />
           ))}
         </div>
-        <h2 className="text text_type_main-medium mt-10 mb-6 main">Начинки</h2>
+        <h2 className="text text_type_main-medium mt-10 mb-6 main" ref={mainsHeader}>Начинки</h2>
         <div className={main__ingredients} ref={mains}>
           {ingredients.filter(ingredient => ingredient.type === 'main').map((ingredient) => (
               <Ingredient
